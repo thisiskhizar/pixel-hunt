f6c9663fc4e1d0fb1ad18d3ddc8fee90dbe338c0
Khizar <khizar.ofc@gmail.com>
2025-07-24 06:37:51 +0500

    Infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,37 @@
-import { Button, SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
-import GameCardContainer from "./GameCardContainer";
-import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
-import type { GameQuery } from "../App";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import type { GameQuery } from "../App";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    isLoading,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-  } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, hasNextPage } =
+    useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   if (error) return <Text>Error: {error.message}</Text>;
 
   return (
-    <>
+    <InfiniteScroll
+      dataLength={
+        data?.pages.reduce((total, page) => total + page.results.length, 0) || 0
+      }
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<Spinner />}
+      endMessage={
+        <Text textAlign="center" marginY={5}>
+          No more games to load
+        </Text>
+      }
+    >
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -47,16 +54,7 @@ const GameGrid = ({ gameQuery }: Props) => {
           </React.Fragment>
         ))}
       </SimpleGrid>
-      {hasNextPage && (
-        <Button
-          onClick={() => fetchNextPage()}
-          isLoading={isFetchingNextPage}
-          margin="10px"
-        >
-          Load More
-        </Button>
-      )}
-    </>
+    </InfiniteScroll>
   );
 };
 export default GameGrid;
