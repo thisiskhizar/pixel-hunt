351354b9ffd11cda7e7725e50ebd0942f84825d8
Khizar <khizar.ofc@gmail.com>
2025-07-24 10:13:22 +0500

    Setup react router

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,77 @@
+import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
+import { useState } from "react";
+
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import NavBar from "../components/NavBar";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+
+import type { Genre } from "../hooks/useGenres";
+import type { Platform } from "../hooks/usePlatforms";
+
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+  sortOrder: string;
+  searchText: string;
+}
+
+const HomePage = () => {
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
+  return ( 
+    <div style={{ padding: "5px" }}>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "250px 1fr",
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
+            }
+          />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" paddingX={5}>
+            <GenreList
+              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+              selectedGenre={gameQuery.genre}
+            />
+          </GridItem>
+        </Show>
+        <GridItem area="main">
+          <Box paddingLeft={2}>
+            <GameHeading gameQuery={gameQuery} />
+            <Flex marginBottom={5}>
+              <Box marginRight={5}>
+                <PlatformSelector
+                  selectedPlatform={gameQuery.platform}
+                  onSelectPlatform={(platform) =>
+                    setGameQuery({ ...gameQuery, platform })
+                  }
+                />
+              </Box>
+              <SortSelector
+                sortOrder={gameQuery.sortOrder}
+                onSelectSortOrder={(sortOrder) =>
+                  setGameQuery({ ...gameQuery, sortOrder })
+                }
+              />
+            </Flex>
+          </Box>
+          <GameGrid gameQuery={gameQuery} />
+        </GridItem>
+      </Grid>
+    </div>
+  );
+};
+export default HomePage;
